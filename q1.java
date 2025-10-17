cb510cab2c199973c9a65b9244c7372924669870
abolfazlrostamtabar-cmyk <abolfazl.rostamtabar@gmail.com>
2025-10-17 15:23:47 +0330

    Add files via upload

--- q1.java
+++ q1.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class q1 {
     public static void main (String[] args){
-        System.out.println("gh2");
+    Scanner input = new Scanner(System.in);
+    int n ;
+    n= input.nextInt();
+    double[] arr = new  double[n];
+    for (int i =0; i<n; i++){
+        arr[i]=input.nextDouble();
+    }
+   double sum =0;
+    for (int i= 0;i<n;i++){
+        sum += arr[i];
+    }
+    System.out.println (sum);
+   
+    double avg;
+    avg= sum /n;
+    System.out.println(avg);
+     double min = arr[0];
+    for(int i=0 ;i<n; i++){
+         if(arr[i]<min)
+            min = arr[i];
+    }
+    System.out.println(min);
+    double max=arr[0];
+    for(int i=0; i<n;i++){
+        if(arr[i]>max)
+            max=arr[i];
+    }
+    System.out.println(max);
+    
     }
 }
